7a72981187dbcfb590fde90222dac07d6407881f
Sachin Kharat <kharatsachin012@gmail.com>
2026-02-25 23:17:01 +0530

    new update

--- JWT/config/db.js
+++ JWT/config/db.js
@@ -3,13 +3,13 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "password", // 👉 XAMPP असेल तर empty ठेव
     database: "auth_system"
 });
 
 db.connect(err => {
     if (err) {
-        console.log("DB Error:", err);
+        console.log("DB Error:", err.message);
     } else {
         console.log("MySQL Connected");
     }
