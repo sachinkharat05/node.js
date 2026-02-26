49823040fbc8dc66b5cbc6ca10ce2e8ab526a548
Sachin Kharat <kharatsachin012@gmail.com>
2026-02-26 22:45:05 +0530

    update server

--- JWT/server.js
+++ JWT/server.js
@@ -11,5 +11,5 @@ const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 4000");
 });
